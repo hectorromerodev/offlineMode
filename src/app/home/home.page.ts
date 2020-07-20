9809db7deb40db9601cb0fb622f27598696cd4c2
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-20 15:21:41 -0700

    UPD Home page updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  users = [];
+
+  constructor(
+    private apiService: ApiService,
+    private plt: Platform
+  ) { }
+
+  ngOnInit() {
+    this.plt.ready().then(() => {
+      this.loadData(true);
+    });
+  }
+
+  loadData(refresh = false, refresher?) {
+    this.apiService.getUsers(refresh).subscribe(res => {
+      this.users = res;
+      if (refresher) {
+        refresher.target.complete();
+      }
+    });
+  }
+
+  updateUser(id) {
+    this.apiService.updateUser(
+      id, {
+      name: 'Hector',
+      job: 'Developer'
+    });
+  }
 
 }
